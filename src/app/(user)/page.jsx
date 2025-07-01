26f08812118db85bb7aa9fc1672470e8a6bb9cae
Kholmurod <khairulloev.kh@gmail.com>
2025-07-01 19:25:17 +0500

    fix bugs v11

---
+++ src/app/(user)/page.jsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+   <>
+   <h1>Home Client</h1>
+   <h1>MedPoint.tj</h1>
+   </>
+  );
+}
--- src/app/page.jsx
+++
@@ -1,9 +0,0 @@
-
-export default function Home() {
-  return (
-   <>
-   <h1>Home Client</h1>
-   <h1>MedPoint.tj</h1>
-   </>
-  );
-}
