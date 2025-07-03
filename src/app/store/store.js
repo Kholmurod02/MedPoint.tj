e93d87b6136abfc1b3b7e17550c027a4294029ec
Kholmurod <khairulloev.kh@gmail.com>
2025-07-03 05:49:24 +0500

    finished crud  of doctors in admin

--- src/app/store/store.js
+++ src/app/store/store.js
@@ -1,13 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { userApi } from '@/entities/user/api/userApi'
+import { doctorApi } from '@/entities/doctor/api/doctorApi'
 import { setupListeners } from '@reduxjs/toolkit/query'
 
 export const store = configureStore({
   reducer: {
     [userApi.reducerPath]: userApi.reducer,
+    [doctorApi.reducerPath]: doctorApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(userApi.middleware),
+    getDefaultMiddleware().concat(
+      userApi.middleware,
+      doctorApi.middleware
+    )
 })
 
 setupListeners(store.dispatch)
---
+++ src/entities/doctor/api/doctorApi.js
@@ -0,0 +1,68 @@
+import { access_token, BASIC_URL } from '@/shared/config/config'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+export const doctorApi = createApi({
+    reducerPath: 'doctorApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: BASIC_URL,
+        prepareHeaders: (headers) => {
+            headers.set('Authorization', `Bearer ${access_token}`)
+            return headers
+        },
+    }),
+    tagTypes: ['doctorApi'],
+    endpoints: (builder) => ({
+
+        // getAllDoctors: builder.query({
+        //     query: (doctor) => `/Doctor/All?Name=${doctor.nameFilter || ""}&Email=${doctor.emailFilter || ""}&Specialization=${doctor.specialization || [""] }&IsActive=${doctor.status || ""}&IsDeleted=${doctor.emailStatus || ""}`,
+        //     providesTags: ['doctorApi']
+        // }),
+
+         getAllDoctors: builder.query({
+            query: (doctor) => `/Doctor/All?Name=${doctor.nameFilter || ""}&Email=${doctor.emailFilter || ""}&IsActive=${doctor.status || ""}&IsDeleted=${doctor.emailStatus || ""}`,
+            providesTags: ['doctorApi']
+        }),
+
+        getDoctorsSpecializations: builder.query({
+            query: () => "/Doctor/specializations",
+            providesTags: ['doctorApi']
+        }),
+
+        getDoctorById: builder.query({
+            query: (id) => `/Doctor/ById?doctorId=${id}`,
+            providesTags: ['doctorApi']
+        }),
+
+        addDoctor: builder.mutation({
+            query: (newDoctor) => ({
+                url: "/Doctor",
+                method: "POST",
+                body: newDoctor
+            }),
+            invalidatesTags: ['doctorApi']
+        }),
+
+        updateDoctor: builder.mutation({
+            query: ({ id, editedDoctor }) => ({
+                url: `/Doctor?id=${id}`,
+                method: "PUT",
+                body: editedDoctor
+            }),
+            invalidatesTags: ['doctorApi']
+        }),
+
+        removeDoctor: builder.mutation({
+            query: (id) => ({
+                url: `/Doctor?doctorId=${id}`,
+                method: "DELETE"
+            }),
+            invalidatesTags: ['doctorApi']
+        })
+
+
+    })
+})
+
+export const { useGetAllDoctorsQuery, useGetDoctorsSpecializationsQuery, useGetDoctorByIdQuery, useAddDoctorMutation, useUpdateDoctorMutation, useRemoveDoctorMutation } = doctorApi
+
+
